0136d0ddf44f5768c0e9d09a3b2b4f2d96b482d3
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-01-23 17:01:57 +0530

    Updated the Get Post

--- pages/api/post/get.ts
+++ pages/api/post/get.ts
@@ -8,10 +8,54 @@ const main = async (req: any, res: any) => {
 
     const postData = await DbModels?.post.find({});
 
-    res.send(postData);
+    const postsUserIds: any[] = [];
+
+    postData?.forEach((post) => {
+      postsUserIds.push(post.userId);
+    });
+
+    const userDatas = await DbModels?.user.find({
+      _id: postsUserIds,
+    });
+
+    const newPostData: {
+      _id: any;
+      userId: any;
+      description: any;
+      url: any;
+      profileUrl: any;
+      postedDate: any;
+      likes: any;
+      comments: any;
+    }[] = [];
+
+    postData?.forEach((post, i) => {
+      const temp = {
+        _id: post._id,
+        userId: post.userId,
+        description: post.description,
+        url: post.url,
+        profileUrl: getUserData(userDatas, post.userId).imgUrl,
+        profileName: getUserData(userDatas, post.userId).name,
+        profileUserName: getUserData(userDatas, post.userId).userName,
+        postedDate: post.postedDate,
+        likes: post.likes,
+        comments: post.comments,
+      };
+
+      newPostData.push(temp);
+    });
+
+    res.send(newPostData);
   } catch (e: any) {
     res.status(500).send(e.message);
   }
+};
+
+const getUserData = (userDatas: any, userId: String) => {
+  return userDatas.find(
+    (user: { _id: String }) => user._id.toString() == userId.toString()
+  );
 };
 
 export default main;
