7b3d9c2a03c7bd07510d1819f2e63bb06d075c53
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-01-29 23:00:10 +0530

    Added Follow and Follow Request

--- pages/api/user/unsign-user/follow.ts
+++ pages/api/user/unsign-user/follow.ts
@@ -3,7 +3,8 @@ import Authenticate from "../../../../Server/middlewares/Authenticate";
 
 const main = async (req: any, res: any) => {
   try {
-    const body = JSON.parse(req.body);
+    // const body = JSON.parse(req.body);
+    const body = req.body;
 
     if (!body.userId) {
       return res.status(406).send({ message: "No Data Given" });
@@ -13,26 +14,83 @@ const main = async (req: any, res: any) => {
 
     const AuthenticateDetail = await Authenticate(req, res);
 
-    const user1Data =
+    const user1Data = (await DbModels?.user.findById(body.userId)) || [];
+
+    const user2Data =
       (await DbModels?.user.findById(AuthenticateDetail?._id)) || [];
 
-    const user2Data = (await DbModels?.user.findById(body.userId)) || [];
-
     if (
-      user1Data.followings.includes(body.userId) &&
-      user2Data.followers.includes(AuthenticateDetail?._id)
+      user2Data.followings.includes(body.userId) &&
+      user1Data.followers.includes(AuthenticateDetail?._id)
     ) {
       return res.send({ message: "Already Followed" });
     } else {
-      await DbModels?.user.findByIdAndUpdate(AuthenticateDetail?._id, {
-        $push: { followings: body.userId },
-      });
+      if (user1Data.isPrivate) {
+        if (
+          user1Data.acceptedFollowerRequests.includes(body.userId) ||
+          user1Data.declinedFollowerRequests.includes(body.userId)
+        ) {
+          await DbModels?.user.findByIdAndUpdate(AuthenticateDetail?._id, {
+            $pull: {
+              acceptedFollowerRequests: body.userId,
+              declinedFollowerRequests: body.userId,
+            },
+          });
 
-      await DbModels?.user.findByIdAndUpdate(body.userId, {
-        $push: { followers: AuthenticateDetail?._id },
-      });
+          if (!user2Data.requestingFollowing.includes(body.userId)) {
+            await DbModels?.user.findByIdAndUpdate(AuthenticateDetail?._id, {
+              $push: { requestingFollowers: body.userId },
+            });
+          }
+        } else {
+          if (!user2Data.requestingFollowing.includes(body.userId)) {
+            await DbModels?.user.findByIdAndUpdate(AuthenticateDetail?._id, {
+              $push: { requestingFollowers: body.userId },
+            });
+          }
+        }
 
-      return res.send({ message: "Followed" });
+        if (
+          user2Data.acceptedFollowingRequests.includes(
+            AuthenticateDetail?._Id
+          ) ||
+          user2Data.declinedFollowingRequests.includes(AuthenticateDetail?._id)
+        ) {
+          await DbModels?.user.findByIdAndUpdate(body.userId, {
+            $pull: {
+              acceptedFollowerRequests: AuthenticateDetail?._id,
+              declinedFollowerRequests: AuthenticateDetail?._id,
+            },
+          });
+
+          if (
+            !user1Data.requestingFollowing.includes(AuthenticateDetail?._id)
+          ) {
+            await DbModels?.user.findByIdAndUpdate(body.userId, {
+              $push: { requestingFollowing: AuthenticateDetail?._id },
+            });
+          }
+        } else {
+          if (
+            !user1Data.requestingFollowing.includes(AuthenticateDetail?._id)
+          ) {
+            await DbModels?.user.findByIdAndUpdate(body.userId, {
+              $push: { requestingFollowing: AuthenticateDetail?._id },
+            });
+          }
+        }
+        return res.send({ message: "Follow Requested" });
+      } else {
+        await DbModels?.user.findByIdAndUpdate(AuthenticateDetail?._id, {
+          $push: { followings: body.userId },
+        });
+
+        await DbModels?.user.findByIdAndUpdate(body.userId, {
+          $push: { followers: AuthenticateDetail?._id },
+        });
+
+        return res.send({ message: "Followed" });
+      }
     }
   } catch (e: any) {
     res.status(500).send(e.message);
