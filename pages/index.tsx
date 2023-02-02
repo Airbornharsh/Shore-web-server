a10cd452a4ded527edab2b38e96944ebe9c2d5ce
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-02-02 20:05:24 +0530

    Changed the Index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,50 @@
-import { DefaultEventsMap } from '@socket.io/component-emitter';
-import { SetStateAction, useEffect, useState } from 'react'
-import io, { Socket } from 'Socket.IO-client'
-let socket: Socket<DefaultEventsMap, DefaultEventsMap>;
+// import { DefaultEventsMap } from '@socket.io/component-emitter';
+// import { SetStateAction, useEffect, useState } from 'react'
+// import io, { Socket } from 'Socket.IO-client'
+// let socket: Socket<DefaultEventsMap, DefaultEventsMap>;
 
-const Home = () => {
-  const [input, setInput] = useState('')
+// const Home = () => {
+//   const [input, setInput] = useState('')
 
-  useEffect(() => {
-    socketInitializer();
-  }, [])
+//   useEffect(() => {
+//     socketInitializer();
+//   }, [])
 
-  const socketInitializer = async () => {
-    await fetch('/api/user/socket');
-    socket = io()
+//   const socketInitializer = async () => {
+//     await fetch('/api/user/socket');
+//     socket = io()
 
-    socket.on('connect', () => {
-      console.log('connected')
-    })
+//     socket.on('connect', () => {
+//       console.log('connected')
+//     })
 
-    socket.on('update-input', msg => {
-      setInput(msg)
-    })
-  }
+//     socket.on('update-input', msg => {
+//       setInput(msg)
+//     })
+//   }
 
-  const onChangeHandler = (e: any) => {
-    setInput(e.target.value)
-    socket.emit('input-change', e.target.value)
-  }
+//   const onChangeHandler = (e: any) => {
+//     setInput(e.target.value)
+//     socket.emit('input-change', e.target.value)
+//   }
 
+//   return (
+//     <input
+//       placeholder="Type something"
+//       value={input}
+//       onChange={onChangeHandler}
+//     />
+//   )
+// }
+
+// export default Home;
+
+import React from 'react'
+
+const index = () => {
   return (
-    <input
-      placeholder="Type something"
-      value={input}
-      onChange={onChangeHandler}
-    />
+    <div>index</div>
   )
 }
 
-export default Home;
+export default index
