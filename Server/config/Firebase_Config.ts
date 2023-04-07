75c856b5d36ade66b2671fd8d41b5dc2ee9b5874
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-04-07 20:59:59 +0530

    Added Message Feature to One

---
+++ Server/config/Firebase_Config.ts
@@ -0,0 +1,42 @@
+const admin = require("firebase-admin");
+const fcm = require("fcm-notification");
+
+let FCM: any;
+let fcmValid = false;
+
+const setFirebase = async () => {
+  if (fcmValid) {
+    console.log("Step -1");
+    return FCM;
+  } else {
+    console.log("Step 0");
+
+    const serviceAccount = {
+      type: process.env.FIREBASE_TYPE,
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+      private_key: process.env.FIREBASE_PRIVATE_KEY,
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      client_id: process.env.FIREBASE_CLIENT_ID,
+      auth_uri: process.env.FIREBASE_AUTH_URI,
+      token_uri: process.env.FIREBASE_TOKEN_URI,
+      auth_provider_x509_cert_url:
+        process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+      client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    };
+
+    serviceAccount.private_key = serviceAccount
+      .private_key!.split(String.raw`\n`)
+      .join("\n");
+
+    const certPath = await admin.credential.cert(serviceAccount);
+
+    FCM = await new fcm(certPath);
+
+    fcmValid = true;
+
+    return FCM;
+  }
+};
+
+export { setFirebase, FCM };
---
+++ pages/api/user/message/one.ts
@@ -0,0 +1,74 @@
+import { DbConnect1 } from "../../../../Server/config/Db_Config";
+import { setFirebase } from "../../../../Server/config/Firebase_Config";
+import Authenticate from "../../../../Server/middlewares/Authenticate";
+
+const main = async (req: any, res: any) => {
+  try {
+    const body = JSON.parse(req.body);
+    // const body = req.body;
+
+    if (!(body.message && body.recieverUserId)) {
+      return res.status(406).send({ message: "No Data Given" });
+    }
+
+    const DbModels = await DbConnect1();
+    const FCM = await setFirebase();
+
+    const AuthenticateDetail = await Authenticate(req, res);
+
+    const recieverUserData = await DbModels?.user.findById(body.recieverUserId);
+
+    if (!recieverUserData) {
+      return res.status(404).send({ message: "User Not Found" });
+    }
+
+    // const newMessage = new DbModels!.message({
+    //     senderId: AuthenticateDetail?._id,
+    //     recieverId: body.userId,
+    //     message: body.message,
+    //     sentDate: Date.now(),
+    // });
+
+    // const messageData = await newMessage.save();
+
+    // await DbModels?.user.findByIdAndUpdate(AuthenticateDetail?._id, {
+    //     $push: { messages: messageData._id },
+    // });
+
+    const tempDeviceTokens = recieverUserData.deviceTokens;
+
+    // recieverUserData.deviceTokens.forEach(async (token: any) => {
+    const message = {
+      // to: token,
+      // token,
+      notification: {
+        title: AuthenticateDetail?.userName,
+        body: body.message,
+      },
+      data: {
+        title: AuthenticateDetail?.userName.toString(),
+        senderUserId: AuthenticateDetail._id.toString(),
+        body: body.message,
+      },
+    };
+
+    FCM.sendToMultipleToken(
+      message,
+      tempDeviceTokens,
+      (err: any, response: any) => {
+        if (err) {
+          console.log("Something has gone wrong!", err);
+        } else {
+          console.log("Successfully sent with response: ", response);
+        }
+      }
+    );
+    // });
+
+    return res.send({ message: "Message Sent" });
+  } catch (e: any) {
+    res.status(500).send(e.message);
+  }
+};
+
+export default main;
