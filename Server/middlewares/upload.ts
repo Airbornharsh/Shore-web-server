3f7c94ce7cacddfc56703c4b4b2741e7dcd857bc
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-04-08 20:38:35 +0530

    Removed Upload

--- Server/middlewares/upload.ts
+++ Server/middlewares/upload.ts
@@ -1,43 +1,49 @@
-import multer from "multer";
-import { GridFsStorage } from "multer-gridfs-storage";
-// import { DbConnect2 } from "../config/Db_Config";
+// import multer from "multer";
+// import { GridFsStorage } from "multer-gridfs-storage";
+// // import { DbConnect2 } from "../config/Db_Config";
 
-const uploadMiddle = (handler: any) => {
-  console.log("call");
-  return async (req: any, res: any, next: any) => {
-    console.log("1");
-    // await DbConnect2();
+// const uploadMiddle = (handler: any) => {
+//   console.log("call");
+//   return async (req: any, res: any, next: any) => {
+//     console.log("1");
+//     // await DbConnect2();
 
-    const storage = new GridFsStorage({
-      url: process.env.DB_POST_URI1 as string,
-      options: { useNewUrlParser: true, useUnifiedTopology: true },
-      file: (req, file) => {
-        const match = ["image/png", "image/jpeg", "image/jpg"];
+//     const storage = new GridFsStorage({
+//       url: process.env.DB_POST_URI1 as string,
+//       options: { useNewUrlParser: true, useUnifiedTopology: true },
+//       file: (req, file) => {
+//         const match = ["image/png", "image/jpeg", "image/jpg"];
 
-        // console.log("2");
+//         // console.log("2");
 
-        const no = Math.floor(Math.random() * 10000000000);
+//         const no = Math.floor(Math.random() * 10000000000);
 
-        if (match.indexOf(file.mimetype) === -1) {
-          const filename = `${Date.now()}-shore-${no}-${file.originalname}`;
-          return filename;
-        }
+//         if (match.indexOf(file.mimetype) === -1) {
+//           const filename = `${Date.now()}-shore-${no}-${file.originalname}`;
+//           return filename;
+//         }
 
-        return {
-          bucketName: "postfile",
-          filename: `${Date.now()}-shore-${no}-${file.originalname}`,
-        };
-      },
-    });
+//         return {
+//           bucketName: "postfile",
+//           filename: `${Date.now()}-shore-${no}-${file.originalname}`,
+//         };
+//       },
+//     });
 
-    await multer({ storage: storage }).single("file")(req, res, () => {
-      console.log(req.file);
-    });
+//     await multer({ storage: storage }).single("file")(req, res, () => {
+//       console.log(req.file);
+//     });
 
-    console.log("3");
+//     console.log("3");
 
-    return handler(req, res);
-  };
-};
+//     return handler(req, res);
+//   };
+// };
 
-export default uploadMiddle;
+// export default uploadMiddle;
+
+const main = ()=>{
+
+}
+
+export default main;
