b75e698a56a8d1b219f95e3d37e7fab8e789624d
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-04-09 16:32:47 +0530

    Updated is valid User and Log out

--- pages/api/user/is-valid-user.ts
+++ pages/api/user/is-valid-user.ts
@@ -3,11 +3,19 @@ import Authenticate from "../../../Server/middlewares/Authenticate";
 
 const main = async (req: any, res: any) => {
   try {
+    const body = req.body;
     const DbModels = await DbConnect1();
 
     const AuthenticateDetail = await Authenticate(req, res);
 
-    const userData = await DbModels?.user.findById(AuthenticateDetail?._id);
+    const userData = await DbModels?.user.findByIdAndUpdate(
+      AuthenticateDetail._id,
+      {
+        $addToSet: { deviceTokens: body.deviceToken },
+      }
+    );
+
+    // const userData = await DbModels?.user.findById(AuthenticateDetail?._id);
 
     if (!userData) {
       return res.status(400).send({ isValid: false, user: null });
---
+++ pages/api/user/logout.ts
@@ -0,0 +1,22 @@
+import { DbConnect1 } from "../../../Server/config/Db_Config";
+import Authenticate from "../../../Server/middlewares/Authenticate";
+
+const main = async (req: any, res: any) => {
+  try {
+    const body = req.body;
+    const DbModels = await DbConnect1();
+
+    const AuthenticateDetail = await Authenticate(req, res);
+
+    await DbModels?.user.findByIdAndUpdate(AuthenticateDetail._id, {
+      $pull: { deviceTokens: body.deviceToken },
+    });
+
+    res.send({ message: "Logged Out" });
+  } catch (e: any) {
+    console.log(e);
+    res.status(500).send(e.message);
+  }
+};
+
+export default main;
