5e94c444c9bef91a595283bbff7fcd7c233d48ec
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-04-20 14:02:30 +0530

    Testing

--- pages/api/user/message/one.ts
+++ pages/api/user/message/one.ts
@@ -7,16 +7,26 @@ const main = async (req: any, res: any) => {
     // const body = JSON.parse(req.body);
     const body = req.body;
 
+    console.log("Step 1");
+
     if (!(body.message && body.recieverUserId && body.currentTime)) {
       return res.status(406).send({ message: "No Data Given" });
     }
+    console.log("Step 2");
 
     const DbModels = await DbConnect1();
+    console.log("Step 3");
+
     const FCM = await setFirebase();
+    console.log("Step 4");
 
     const AuthenticateDetail = await Authenticate(req, res);
 
+    console.log("Step 5");
+
     const recieverUserData = await DbModels?.user.findById(body.recieverUserId);
+
+    console.log("Step 6");
 
     if (!recieverUserData) {
       return res.status(404).send({ message: "User Not Found" });
@@ -35,9 +45,13 @@ const main = async (req: any, res: any) => {
     //     $push: { messages: messageData._id },
     // });
 
+    console.log("Step 7");
+
     const tempDeviceTokens = recieverUserData.deviceTokens;
 
     // recieverUserData.deviceTokens.forEach(async (token: any) => {
+
+    console.log("Step 8");
 
     const message = {
       // to: token,
@@ -54,17 +68,22 @@ const main = async (req: any, res: any) => {
       },
     };
 
+    console.log("Step 9");
+
     FCM.sendToMultipleToken(
       message,
       tempDeviceTokens,
       (err: any, response: any) => {
         if (err) {
           console.log("Something has gone wrong!", err);
+          console.log("Step 10");
         } else {
           console.log("Successfully sent with response: ", response);
+          console.log("Step 11");
         }
       }
     );
+    console.log("Step 12");
 
     const newMessage = new DbModels!.message({
       from: AuthenticateDetail?._id,
@@ -74,7 +93,11 @@ const main = async (req: any, res: any) => {
       type: body.type ? body.type.trim() : "text",
     });
 
+    console.log("Step 13");
+
     const data = await newMessage.save();
+
+    console.log("Step 14");
 
     return res.send({ message: "Message Sent" });
   } catch (e: any) {
