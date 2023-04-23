75a4d45991f40312b920a4fea18d8f7ecafaef0e
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-04-23 11:11:52 +0530

    Added Extra Detail in Post List

--- pages/api/user/liked/post/list.ts
+++ pages/api/user/liked/post/list.ts
@@ -21,13 +21,59 @@ const main = async (req: any, res: any) => {
         })
         .sort({ $natural: -1 });
 
-      res.send(postData);
+      const postsUserIds: any[] = [];
+
+      postData?.forEach((post) => {
+        postsUserIds.push(post.userId);
+      });
+
+      const userDatas = await DbModels?.user.find({
+        _id: postsUserIds,
+      });
+
+      const newPostData: {
+        _id: any;
+        userId: any;
+        description: any;
+        url: any;
+        profileUrl: any;
+        profileName: any;
+        profileUserName: any;
+        postedDate: any;
+        likes: any;
+        comments: any;
+      }[] = [];
+
+      postData?.forEach((post, i) => {
+        const temp = {
+          _id: post._id,
+          userId: post.userId,
+          description: post.description,
+          url: post.url,
+          profileUrl: getUserData(userDatas, post.userId).imgUrl,
+          profileName: getUserData(userDatas, post.userId).name,
+          profileUserName: getUserData(userDatas, post.userId).userName,
+          postedDate: post.postedDate,
+          likes: post.likes,
+          comments: post.comments,
+        };
+
+        newPostData.push(temp);
+      });
+
+      res.send(newPostData);
     } else {
       res.send([]);
     }
   } catch (e: any) {
     res.status(500).send({ message: e.message });
   }
+};
+
+const getUserData = (userDatas: any, userId: String) => {
+  return userDatas.find(
+    (user: { _id: String }) => user._id.toString() == userId.toString()
+  );
 };
 
 export default main;
