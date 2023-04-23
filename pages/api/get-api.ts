d56918254423d51a2c57a4496b668a97ac064f6e
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-04-23 11:41:37 +0530

    Added Api Link

---
+++ pages/api/get-api.ts
@@ -0,0 +1,9 @@
+const main = async (req: any, res: any) => {
+  try {
+    res.send({ api: "https://shore.vercel.app" });
+  } catch (e: any) {
+    res.status(500).send({ message: e.message });
+  }
+};
+
+export default main;
