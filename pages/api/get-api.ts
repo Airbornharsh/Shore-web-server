839b62954db41f8a0ad5c415bb9088dba3bb7c72
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-04-23 11:45:16 +0530

    Updated get Api

--- pages/api/get-api.ts
+++ pages/api/get-api.ts
@@ -1,6 +1,6 @@
 const main = async (req: any, res: any) => {
   try {
-    res.send({ api: "https://shore.vercel.app" });
+    res.send({ api_uri: "https://shore.vercel.app" });
   } catch (e: any) {
     res.status(500).send({ message: e.message });
   }
